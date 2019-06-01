84dc97fa57a5e8938ff01b4d2b26bef6e7935635
Jamie Sumner <jamiesumner@Jamies-MacBook-Air.local>
2019-06-01 18:32:51 -0400

    added concert-this function and added switch

--- liri.js
+++ liri.js
@@ -1,40 +1,70 @@
 require("dotenv").config()
-var Spotify = require("node-spotify-api");
-var keys = require("./keys");
-var axios = require("axios");
+const Spotify = require("node-spotify-api");
+const keys = require("./keys");
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
 
-var spotify = new Spotify({
+const spotify = new Spotify({
     id: keys.spotify.id,
     secret: keys.spotify.secret
 });
 
-var command = process.argv[2];
-var userSearch = "";
+const command = process.argv[2];
+const input = process.argv[3];
 
-for (var i = 3; i < process.argv.length; i++) {
-    userSearch += process.argv[i] + " "
+switch (command) {
+
+    case "spotify-this-song":
+        spotifySearch();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
 }
 
-if (command === "spotify-this-song") {
-    spotifySearch(userSearch)
-} else if (command === "movie-this") {
-    movieThis(userSearch)
-}
-
-function spotifySearch(song) {
-    spotify.search({ type: 'track', query: song }, function (err, data) {
+function spotifySearch() {
+    spotify.search({ type: 'track', query: input }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
+        // only console logs all the data - need to specify what to console log
         console.log(JSON.stringify(data, null, 2));
     });
 }
 
-function movieThis(movie) {
-    axios.get('http://www.omdbapi.com/?apikey=' + keys.OMDB.key + '&s=' + movie)
+function movieThis() {
+    axios.get('https://www.omdbapi.com/?t=' + input + '&apikey=trilogy')
         .then(function (response) {
-            console.log(response.data.Search[0]);
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language:: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function concertThis() {
+    axios.get('https://rest.bandsintown.com/artists/' + input + '/events?app_id=codingbootcamp')
+        .then(function (response) {
+            console.log("Venue: " + response.data[0].venue.name);
+            console.log("Location: " + response.data[0].venue.city);
+            console.log("Date: " + moment(response.data[0].datetime).format('MM/DD/YYYY'));
         })
         .catch(function (error) {
             console.log(error);
